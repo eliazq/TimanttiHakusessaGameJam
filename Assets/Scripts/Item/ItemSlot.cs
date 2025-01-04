73b101eb9097a9a8ca3924d4db6616195938238b
Elias H <69460338+eliazq@users.noreply.github.com>
2025-01-04 22:08:32 +0200

    Inventory UI + Item slot Updates

--- Assets/Scripts/Item/ItemSlot.cs
+++ Assets/Scripts/Item/ItemSlot.cs
@@ -69,6 +69,8 @@ public class ItemSlot : MonoBehaviour
     }
     public static void Select(ItemSlot itemSlot)
     {
+        if (!itemSlot.hasItem) return;
+
         itemSlot.Selected = true;
         itemSlot.ShowSelectedVisual();
 
